28ef781bc26abc4cadaf0d4ede55c90d15adeea2
Roland Nilsson <roland@minerva.net>
2022-11-18 15:13:42 +0100

    First attempt at an optimized version of conv_reduce

--- R/distance.R
+++ R/distance.R
@@ -141,14 +141,22 @@ max_sim <- function(sim_values)
 #'
 conv_distance <- function(mi_data, x, y, e, distance, default = Inf)
 {
-  conv_reduce(mi_data, x, y, e, distance, min_dist)
+  conv_reduce(mi_data, x, y, e, distance,
+              function(dist_values) min_dist(dist_values, default))
 }
 
-min_dist <- function(dist_values)
+min_dist <- function(dist_values, default = Inf)
 {
   if(length(dist_values) == 0)
     return(default)
   return(min(dist_values, na.rm = TRUE))
+}
+
+# could perhaps be moved to midata.R
+get_avg_mids_by_size <- function(mi_data, n_atoms, e)
+{
+  index <- get_peak_index_n_atoms(mi_data, n_atoms)
+  return(sapply(index, function(i) get_avg_mid(mi_data, i, e)))
 }
 
 
@@ -165,7 +173,6 @@ min_dist <- function(dist_values)
 #' @param e experiment index
 #' @param f A function f(x, y) taking two MIDs.
 #' @param g a function g taking a vector of values f1, f2, ...
-#' @param default value to use when there is no possible convolution
 #' @returns the resulting value g(f1, f2, ...)
 #' @export
 
@@ -178,7 +185,6 @@ conv_reduce <- function(mi_data, x, y, e, f, g)
   if(n_atom_x > n_atom_y) {
     return(conv_reduce(mi_data, y, x, e, f, g))
   }
-
   # find the MIDs of metabolite with index x, y from experiment e
   mid_x <- get_avg_mid(mi_data, x, e)
   mid_y <- get_avg_mid(mi_data, y, e)
@@ -189,13 +195,10 @@ conv_reduce <- function(mi_data, x, y, e, f, g)
   }
   else {
     # x is strictly smaller than y
-    # index of metabolites z to convolute with
-    carbon_diff <- n_atom_y - n_atom_x
-    z_index <- get_peak_index_n_atoms(mi_data, carbon_diff)
-
-    if (length(z_index) > 0) {
-      # get all MIDs z
-      mids_z = sapply(z_index, function(i) get_avg_mid(mi_data, i, e))
+    # get MIDs of metabolites z to convolute with
+    n_atom_z <- n_atom_y - n_atom_x
+    mids_z <- get_avg_mids_by_size(mi_data, n_atom_z, e)
+    if(length(mids_z) > 0) {
       # compute all convolutions x*z for each z
       convolutions <- convolute_cols(mid_x, mids_z)
       # calculate f between the larger metabolite and all possible convolutions
@@ -209,4 +212,65 @@ conv_reduce <- function(mi_data, x, y, e, f, g)
     }
   }
 }
+
+
+#' Same as conv_reduce, but for all pairs x*y, returns a matrix.
+#' this avoids recalculating convolutions
+#' @param mi_data the MIdata object
+#' @param e experiment index
+#' @param f A function f(x, y) taking two MIDs.
+#' @param g a function g taking a vector of values f1, f2, ...
+#' @returns the matrix of g(f(x,y) ...) values for all x,y
+#' @export
+conv_reduce_all <- function(mi_data, e, f, g)
+{
+  # allocate square matrix
+  n_met <- length(mi_data$peak_ids)
+  result <- matrix(0, nrow = n_met, ncol = n_met)
+  # unique metabolite sizes
+  n_atoms <- unique(mi_data$peak_n_atoms)
+  # loop over x
+  for(i in 1:length(n_atoms)) {
+    n_atoms_x <- n_atoms[[i]]
+    for(x in get_peak_index_n_atoms(mi_data, n_atoms_x)) {
+
+      # MIDs of metabolite with index x
+      mid_x <- get_avg_mid(mi_data, x, e)
+
+      for(j in i : length(n_atoms)) {
+        n_atoms_y <- n_atoms[[j]]
+        # get all MIDs y
+        y_index <- get_peak_index_n_atoms(mi_data, n_atoms_y)
+        mids_y <- sapply(y_index, function(i) get_avg_mid(mi_data, i, e))
+        if(n_atoms_x == n_atoms_y) {
+          # for metabolites y of same size as x, just calculate f(x,y)
+          result[x, y_index] <- apply(mids_y, MARGIN = 2,
+                                      function(y) g(c(f(mid_x, y))))
+        }
+        else {
+          # size of metabolites z to convolute with x
+          n_atoms_z <- n_atoms_y - n_atoms_x
+          mids_z <- get_avg_mids_by_size(mi_data, n_atoms_z, e)
+
+          if(length(mids_z) > 0) {
+            # compute all convolutions x*z for each z
+            convolutions <- convolute_cols(mid_x, mids_z)
+            # for each y, calculate g(f(x*z, y)) over all convolutions x*z
+            result[x, y_index] <- apply(mids_y, MARGIN = 2,
+                     function(y) g(apply(convolutions, MARGIN = 2, f, y)))
+          }
+          else {
+            # no metabolites z to convolute x with
+            result[x, y_index] <- rep(g(c()), n = length(y_index))
+          }
+        }
+        result[y_index, x] <- result[x, y_index]
+      }
+    }
+  }
+  return(result)
+}
+
+
+
 
