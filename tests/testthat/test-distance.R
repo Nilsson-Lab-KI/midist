8680fbb62e6408006dcc554b435875cec4ee0405
Roland Nilsson <roland@minerva.net>
2023-02-14 08:38:27 +0100

    Added test case from HMEC data where the current implementation fails

--- tests/testthat/test-distance.R
+++ tests/testthat/test-distance.R
@@ -135,15 +135,17 @@ test_conv_reduce_all <- function(mi_data, f, g)
 {
   # matrix from conv_reduce_all
   conv_all_mat <- conv_reduce_all(mi_data, 1, f, g)
+  # make sure matrix is symmetric
+  expect_true(isSymmetric(conv_all_mat))
 
   # check against conv_reduce element by element
   n_peaks <- length(mi_data$peak_ids)
   for(x in 1:n_peaks) {
     for(y in 1:n_peaks) {
-      conv_elem <- conv_reduce(mi_data, x, y, 1, f, g)
       conv_all_elem <- with_attr(conv_all_mat[x,y], "index",
                                  attr(conv_all_mat, "index")[x,y])
-    expect_equal(conv_all_elem, conv_elem)
+      expect_equal(conv_all_elem,
+                   conv_reduce(mi_data, !!x, !!y, 1, f, g))
     }
   }
 }
@@ -168,5 +170,44 @@ test_that("conv_reduce_all returns a valid distance matrix for example-2", {
 })
 
 
+# test case from HMEC data
+adp_mid <- c(0.3739468302, 0.0868470331, 0.1394170159, 0.1083851249,
+             0.2638703387, 0.0266737617, 0.0008598955, 0.0000000000,
+             0.0000000000, 0.0000000000, 0.0000000000)
 
+gthrd_mid <- c(2.910509e-01, 4.279995e-02, 6.118703e-01, 5.421727e-02,
+               4.515372e-06, 5.363846e-05, 0.000000e+00, 3.399253e-06,
+               0.000000e+00, 0.000000e+00, 0.000000e+00)
+
+ibutcrn_mid <- c(8.863661e-01, 1.040638e-01, 9.447574e-03, 0.000000e+00,
+                 6.969751e-05, 7.760243e-06, 0.000000e+00, 0.000000e+00,
+                 0.000000e+00, 0.000000e+00, 4.509453e-05, 0.000000e+00)
+
+nad1_mid <- c(3.185700e-01, 1.165741e-01, 1.398212e-01, 1.157578e-01,
+              2.549923e-01, 5.174851e-02, 2.379705e-03, 8.610149e-05,
+              0.000000e+00, 7.030201e-05, 0.000000e+00, 0.000000e+00,
+              0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00,
+              0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00,
+              0.000000e+00, 0.000000e+00)
+
+nad2_mid <- c(3.116997e-01, 1.203842e-01, 1.414667e-01, 1.142038e-01,
+              2.490341e-01, 5.413205e-02, 8.889942e-03, 9.395356e-05,
+              0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00,
+              0.000000e+00, 0.000000e+00, 0.000000e+00, 4.062245e-05,
+              5.491461e-05, 0.000000e+00, 0.000000e+00, 0.000000e+00,
+              0.000000e+00, 0.000000e+00)
+
+# example-1: carbon numbers are sorted
+peak_areas_example_hmec <- data.frame(
+  Metabolite = c(rep("adp", 10+1), rep("gthrd" ,10+1), rep("ibutcrn", 11+1), rep("nad1", 21+1), rep("nad2", 21+1)),
+  Formula = c(rep("adp", 10+1), rep("gthrd" ,10+1), rep("ibutcrn", 11+1), rep("nad1", 21+1), rep("nad2", 21+1)),
+  exp1 = c(adp_mid, gthrd_mid, ibutcrn_mid, nad1_mid, nad2_mid)
+)
+
+# create MIData object
+midata_hmec <- MIData(peak_areas_example_hmec, exp_names = "exp1")
+
+test_that("conv_reduce_all returns a valid matrix for HMEC example", {
+  test_conv_reduce_all(midata_hmec, euclidean_dist_sq, min_nonempty)
+})
 
