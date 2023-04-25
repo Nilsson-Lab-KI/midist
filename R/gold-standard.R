48766456cdef36bd2aa8970014001f5ceb6a8f5e
dsecilmis <deniz.secilmis@ki.se>
2023-04-25 17:47:31 +0200

    faster continuous accuracy

--- R/gold-standard.R
+++ R/gold-standard.R
@@ -136,45 +136,45 @@ get_global_percentile_accuracy <- function(pairwise_matrix, gold_standard, measu
 
 #' @export
 get_continuous_accuracy <- function(pairwise_matrix, gold_standard, measure, noise, experiment, subset_size){
-  # create an empty data frame to store the accuracy results
-  accuracy_df <- data.frame(measure = NA, noise = NA,
-                            tpr = NA, fpr = NA, precision = NA, fdr = NA, f1_score = NA,
-                            experiment = NA, subset_size = NA)
-  
+
   # make sure the diagonal is NA
   diag(pairwise_matrix) <- NA
+  diag(gold_standard) <- NA
   
-  # now we go from full to empty
-  tpr <- c(); fpr <- c(); precision <- c(); fdr <- c(); f1_score <- c()
+  # put both matrices in a vector
+  gs_vector <- as.vector(gold_standard)
+  pm_vector <- as.vector(pairwise_matrix)
   
-  thresholds <- unique(as.vector(pairwise_matrix))[-which(is.na(unique(as.vector(pairwise_matrix))))]
-  thresholds <- thresholds[order(thresholds, decreasing = T)]
+  # diagonals in gold standard are NAs - remove them from both vectors
+  gs_na_ind <- which(is.na(gs_vector))
+  pm <- pm_vector[-gs_na_ind]
+  gs <- gs_vector[-gs_na_ind]
   
-  for (p in 1:length(thresholds)){
-    # assign NAs to all values above a given threshold
-    filtered_pm <- pairwise_matrix
-    filtered_pm[which(pairwise_matrix > thresholds[p])] <- NA
-    # binarize
-    filtered_pm[which(is.na(filtered_pm))] <- 0
-    filtered_pm[which(filtered_pm != 0)] <- 1
-    
-    # now we have to binary matrices to compare to each other: from gs-fraction and MID-distance
-    accuracy <- calc_accuracy(gold_standard, filtered_pm)
-    tpr[p] <- accuracy$tpr
-    fpr[p] <- accuracy$fpr
-    precision[p] <- accuracy$precision
-    fdr[p] <- accuracy$fdr
-    f1_score[p] <- accuracy$f1_score
-  }
+  # now assign a maximal distance to all remaining NAs of pm
+  pm[which(is.na(pm))] <- max(pm, na.rm = T)
   
-  accuracy_df <- rbind(accuracy_df, data.frame(measure = measure, noise = noise,
-                                               tpr = tpr, fpr = fpr, precision = precision, fdr = fdr, f1_score = f1_score,
-                                               experiment = experiment, subset_size = subset_size))
+  # sort matrices
+  order_ind <- order(pm)
+  pm <- pm[order_ind]
+  gs <- gs[order_ind]
   
-  # remove the NA line
-  accuracy_df <- accuracy_df[-1,]
   
-  return(accuracy_df)
+  tp <- cumsum(gs)
+  fp <- cumsum(abs(1-gs))
+  # tn <- c(0:(length(gs)-1))*fp
+  # fn <- c(0:(length(gs)-1))*tp
+  
+  # Compute precision and recall for a single decision threshold
+  threshold <- 1:length(gs)
+  precision <- tp[threshold] / (tp[threshold] + fp[threshold])
+  recall <- tp[threshold] / max(tp)
+  # precision <-  sapply(1:length(gs), function(x, tp, fp) tp[x] / (tp[x] + fp[x]), tp, fp) 
+  # recall <-  sapply(1:length(gs), function(x, tp) tp[x] / max(tp), tp) 
+
+  
+  return(data.frame(measure = measure, noise = noise,
+                    tpr = recall, precision = precision,
+                    experiment = experiment, subset_size = subset_size))
   
 }
 
