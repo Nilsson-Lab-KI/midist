a0c82792a286d6f24629f890bc42d8a2fd0083f2
dsecilmis <deniz.secilmis@ki.se>
2023-08-08 11:00:04 +0200

    commented out the tests for normalize_mid

--- tests/testthat/test-midata.R
+++ tests/testthat/test-midata.R
@@ -212,33 +212,33 @@ test_that("midata_subset works correctly", {
 # MID normalization -- move this to test-mid.R ?
 #
 
-test_that("normalized MIDs sum to 1", {
-  # example "peak area" data, each column an MID
-  peak_areas <- matrix(
-    c(
-      1, 0,
-      3, 2,
-      7, 0
-    ),
-    nrow = 3, ncol = 2
-  )
-  mids <- normalize_mids(peak_areas)
-  expect_equal(colSums(mids), c(1, 1))
-})
+# test_that("normalized MIDs sum to 1", {
+#   # example "peak area" data, each column an MID
+#   peak_areas <- matrix(
+#     c(
+#       1, 0,
+#       3, 2,
+#       7, 0
+#     ),
+#     nrow = 3, ncol = 2
+#   )
+#   mids <- normalize_mids(peak_areas)
+#   expect_equal(colSums(mids), c(1, 1))
+# })
 
-test_that("normalizing zero vector gives zero vector", {
-  # example "peak area" data, each column an MID
-  peak_areas <- matrix(
-    c(
-      0,
-      0,
-      0
-    ),
-    nrow = 3, ncol = 1
-  )
-  mids <- normalize_mids(peak_areas)
-  expect_equal(colSums(mids), c(0))
-})
+# test_that("normalizing zero vector gives zero vector", {
+#   # example "peak area" data, each column an MID
+#   peak_areas <- matrix(
+#     c(
+#       0,
+#       0,
+#       0
+#     ),
+#     nrow = 3, ncol = 1
+#   )
+#   mids <- normalize_mids(peak_areas)
+#   expect_equal(colSums(mids), c(0))
+# })
 
 
 test_that("false isotope removal works correctly", {
